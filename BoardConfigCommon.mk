58e07f9a60bf3fe1b8ad91fea83fcb42759469ee
Shikhar <krtonia@gmail.com>
2023-07-17 22:30:24 +0530

    sdm845-common: Drop trb clang

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -47,8 +47,6 @@ TARGET_KERNEL_SOURCE := kernel/xiaomi/sdm845
 TARGET_KERNEL_ADDITIONAL_FLAGS += HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 TARGET_KERNEL_ADDITIONAL_FLAGS := LLVM_IAS=1
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CLANG_VERSION := trb_clang
-TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/trb_clang
 
 # Platform
 TARGET_BOARD_PLATFORM := sdm845
